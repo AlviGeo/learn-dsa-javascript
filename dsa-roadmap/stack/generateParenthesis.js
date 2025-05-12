58c02936dde30b0fa3f6358d427dcbc8002d623c
AlviGeo <alvigeovan29@gmail.com>
2025-05-12 22:49:40 +0700

    feat: solve generate parenthesis leet code problem

---
+++ dsa-roadmap/stack/generateParenthesis.js
@@ -0,0 +1,27 @@
+class Solution {
+  /**
+   * @param {number} n
+   * @return {string[]}
+   */
+  generateParenthesis(n) {
+    const result = [];
+
+    function backtrack(str, open, close) {
+      if (str.length === n * 2) {
+        result.push(str);
+        return;
+      }
+
+      if (open < n) {
+        backtrack(str + "(", open + 1, close);
+      }
+
+      if (close < open) {
+        backtrack(str + ")", open, close + 1);
+      }
+    }
+
+    backtrack("", 0, 0);
+    return result;
+  }
+}
